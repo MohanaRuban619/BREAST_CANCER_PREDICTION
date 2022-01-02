eed0d9d6d0e4c471862e5ea3ee90837d6f11d566
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-02 11:17:35 +0530

    Delete first_app directory

--- first_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Input
-
-admin.site.register(Input)
-
-# Register your models here.
--- first_app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Input
-
-class InputSerializers(serializers.ModelSerializer):
-    class meta:
-        model=Input
-        fields ='__all__'
